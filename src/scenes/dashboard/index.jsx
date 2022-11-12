10f2000c5690a493842a70777a02049938c1afff
anita-brown <efeaniita@gmail.com>
2022-11-12 23:58:54 +0100

    chore: created header component

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Header from '../../components/Header'
 
 const Dashboard = () => {
   return (
     <div>
-      
+      <Header title="DASHBOARD" subtitle="Welcome to your dashboard"/>
     </div>
   )
 }
