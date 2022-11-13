18fe22004e1262ccde61f7f498f2976bd620fe63
anita-brown <efeaniita@gmail.com>
2022-11-13 22:32:49 +0100

    chore: implemented data grid for contacts

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import TopBar from "./scenes/global/TopBar";
 import SideBar from "./scenes/global/SideBar";
 import Dashboard from "./scenes/dashboard";
 import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
 // import Invoices from "./scenes/invoices";
-// import Contacts from "./scenes/contacts";
 // import Bar from "./scenes/bar";
 // import Form from "./scenes/form";
 // import Line from "./scenes/line";
@@ -30,8 +30,8 @@ function App() {
             <Routes>
               <Route path="/" element={<Dashboard />} />
               <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
               {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/form" element={<Form />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
--- src/scenes/team/index.jsx
+++ src/scenes/team/index.jsx
@@ -2,10 +2,8 @@ import { Typography, Box, useTheme } from "@mui/material";
 import { DataGrid } from "@mui/x-data-grid";
 import { tokens } from "../../theme";
 import { mockDataTeam } from "../../data/mockData";
-// import AdminPanelSettingsOutlinedIcon from '@mui/icons-material/AdminPanelSettingsOutlinedIcon'
 import AdminPanelSettingsOutlinedIcon from "@mui/icons-material/AdminPanelSettingsOutlined";
 import LockOpenOutlinedIcon from "@mui/icons-material/LockOpenOutlined";
-// import LockOpenOutlinedIcon from '@mui/icons-material/LockOpenOutlinedIcon'
 import SecurityOutlinedIcon from "@mui/icons-material/SecurityOutlined";
 import Header from "../../components/Header";
 
